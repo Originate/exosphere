1d851a859ca3ebf6a40fb49388998275c57e4096
Kevin Goslar <kevin.goslar@gmail.com>
2016-07-03 21:53:27 -0700

    Stop Cucumber at the first error

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "default": "--compiler ls:livescript -r features --strict --snippet-syntax node_modules/cucumber-snippets-livescript"
+  "default": "--compiler ls:livescript -r features --fail-fast --strict --snippet-syntax node_modules/cucumber-snippets-livescript"
 }
