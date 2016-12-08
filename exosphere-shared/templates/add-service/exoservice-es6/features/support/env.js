34458fb3c9c9798e3a06290e0f835fe6c84e46a4
Thomas Rushton <trushton134@gmail.com>
2016-12-08 15:19:07 -0800

    Remove old 'closePort' call in templates

--- exosphere-shared/templates/add-service/exoservice-es6/features/support/env.js
+++ exosphere-shared/templates/add-service/exoservice-es6/features/support/env.js
@@ -6,6 +6,5 @@ module.exports = function() {
   this.setDefaultTimeout(1000)
   this.exocom && this.exocom.close()
   this.process && this.process.close()
-  this.process && this.process.closePort()
 
 }
