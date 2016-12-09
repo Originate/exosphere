979231e25c629dde542644914ff0b105b000b1c6
Thomas Rushton <trushton134@gmail.com>
2016-12-08 17:05:16 -0800

    Remove close-port calls

--- exosphere-shared/templates/add-service/exoservice-es6/features/support/env.js
+++ exosphere-shared/templates/add-service/exoservice-es6/features/support/env.js
@@ -6,6 +6,4 @@ module.exports = function() {
   this.setDefaultTimeout(1000)
   this.exocom && this.exocom.close()
   this.process && this.process.close()
-  this.process && this.process.closePort()
-
 }
