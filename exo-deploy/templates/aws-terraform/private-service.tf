c8e6a3af47f04e2b812d14f71457ff32075dd5c1
Hugo Ho <hugoho@Hugos-MacBook-Pro.local>
2017-01-24 17:13:16 -0800

    shorten aws-resource names
    closes #176

--- exo-deploy/templates/aws-terraform/private-service.tf
+++ exo-deploy/templates/aws-terraform/private-service.tf
@@ -1,8 +1,8 @@
 
-module "{{name}}-service" {
+module "{{name}}" {
   source = "./private-services"
 
-  name = "exosphere-{{name}}-service"
+  name = "{{name}}"
   cluster_id = "${module.public-cluster.cluster_id}"
   security_groups = "${aws_security_group.public.id}"
   subnet_ids = "${module.vpc.public_subnet_id}" /* TODO: make private */
