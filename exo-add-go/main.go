9f24696538cb28650421205a612664440f29ed1f
Cheahuychou Mao <cmao18@mit.edu>
2017-06-27 17:12:00 -0700

    get the command to run

--- exo-add-go/main.go
+++ exo-add-go/main.go
@@ -4,44 +4,45 @@ import (
 	"bufio"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"strconv"
 	"strings"
-	"text/template"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"github.com/tcnksm/go-input"
 	"gopkg.in/yaml.v2"
 )
+
+var serviceRole, serviceType, description, author, templatePath, modelName, protectionLevel string
+var serviceConfig ServiceConfig
 
 var rootCmd = &cobra.Command{
 	Use:   "exo add",
 	Short: "\nUsage: #{cyan 'exo add'} #{blue '[<entity-name>]'}\n\nAdds a new service to the current application.\nThis command must be called in the root directory of the application.\n\noptions: #{blue '--service-role=[<service-role>] --service-type=[<service-type>] --template-name=[<template-name>] --model-name=[<model-name>] --protection-level=[<protection-level>] --description=[<description>]'}",
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
 		if len(serviceRole) == 0 {
-			serviceRole = ask("Role of the service to create:", true)
+			serviceRole = ask(reader, "Role of the service to create:", true)
 		}
 		if len(serviceType) == 0 {
-			serviceType = ask("Type of the service to create:", true)
+			serviceType = ask(reader, "Type of the service to create:", true)
 		}
 		if len(description) == 0 {
-			description = ask("Description:", false)
+			description = ask(reader, "Description:", false)
 		}
 		if len(author) == 0 {
-			author = ask("Author:", true)
+			author = ask(reader, "Author:", true)
 		}
 		if len(templatePath) == 0 {
 			templates := []string{} // read .exosphere for choices
-			templatePath = choose("Template:", templates)
+			templatePath = choose(reader, "Template:", templates)
 		}
 		if len(modelName) == 0 {
-			modelName = ask("Name of the data model:", false)
+			modelName = ask(reader, "Name of the data model:", false)
 		}
 		if len(protectionLevel) == 0 {
-			protectionLevel = choose("Protection level:", []string{"public", "private"})
+			protectionLevel = choose(reader, "Protection level:", []string{"public", "private"})
 		}
-		serviceConfig := ServiceConfig{serviceRole, serviceType, description, author, templatePath, modelName, protectionLevel}
+		serviceConfig = ServiceConfig{serviceRole, serviceType, description, author, templatePath, modelName, protectionLevel}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 1 && args[0] == "help" {
@@ -60,96 +61,106 @@ var rootCmd = &cobra.Command{
 		err = yaml.Unmarshal(yamlFile, &appConfig)
 		checkForService(serviceConfig.ServiceRole, getExistingServices(appConfig.Services))
 
-
 	},
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&serviceRole, "service-role")
-	rootCmd.PersistentFlags().StringVar(&serviceType, "service-type")
-	rootCmd.PersistentFlags().StringVar(&description, "description")
-	rootCmd.PersistentFlags().StringVar(&author, "author")
-	rootCmd.PersistentFlags().StringVar(&templatePath, "template-name")
-	rootCmd.PersistentFlags().StringVar(&modelName, "model-name")
-	rootCmd.PersistentFlags().StringVar(&protectionLevel, "protection-level")
+	rootCmd.PersistentFlags().StringVar(&serviceRole, "service-role", "", "")
+	rootCmd.PersistentFlags().StringVar(&serviceType, "service-type", "", "")
+	rootCmd.PersistentFlags().StringVar(&description, "description", "", "")
+	rootCmd.PersistentFlags().StringVar(&author, "author", "", "")
+	rootCmd.PersistentFlags().StringVar(&templatePath, "template-name", "", "")
+	rootCmd.PersistentFlags().StringVar(&modelName, "model-name", "", "")
+	rootCmd.PersistentFlags().StringVar(&protectionLevel, "protection-level", "", "")
 }
 
+// Dependency is an unexpected type
 type Dependency struct {
-	Name string
+	Name    string
 	Version string
-	Config DependencyConfig
+	Config  DependencyConfig
 }
 
+// DependencyConfig is an unexpected type
 type DependencyConfig struct {
-	Ports []string
-	Volumes []string
-	OnlineText string
+	Ports                 []string
+	Volumes               []string
+	OnlineText            string
 	DependencyEnvironment map[string]string
-	ServiceEnvironment map[string]string
+	ServiceEnvironment    map[string]string
 }
 
+// Service is an unexpected type
 type Service struct {
-	Namespace string
-	Location string
+	Namespace   string
+	Location    string
 	DockerImage string
 }
 
+// AppConfig is an unexpected type
 type AppConfig struct {
-	Name string
-	Description string
-	Version string
+	Name         string
+	Description  string
+	Version      string
 	Dependencies []Dependency
-	Services map[string]map[string]Service
+	Services     map[string]map[string]Service
 }
 
+// ServiceConfig is an unexpected type
 type ServiceConfig struct {
-  ServiceRole, ServiceType, Description, Author, TemplatePath, ModelName, ProtectionLevel string
+	ServiceRole, ServiceType, Description, Author, TemplatePath, ModelName, ProtectionLevel string
 }
 
-func ask(query string, required bool) string {
-  ui := &input.UI{
-    Writer: os.Stdout,
-    Reader: os.Stdin,
-  }
-  answer, err := ui.Ask(query, &input.Options{
-    Required: required,
-    Loop:     true,
-  })
-  if err != nil {
-    panic(err)
-  }
-  return answer
+func ask(reader *bufio.Reader, query string, required bool) string {
+	fmt.Print(query)
+	answer, err := reader.ReadString('\n')
+	answer = strings.TrimSpace(answer)
+	if err != nil {
+		panic(err)
+	}
+	if len(answer) == 0 && required {
+		fmt.Println("expect a non-empty string")
+		return ask(reader, query, required)
+	}
+	return answer
 }
 
-func choose(query string, options []string) string {
-	ui := &input.UI{
-    Writer: os.Stdout,
-    Reader: os.Stdin,
-  }
-  answer, err := &ui.Select(query, []string options, &input.Options{
-    Required: true,
-    Loop:     true,
-  })
-  if err != nil {
-    panic(err)
-  }
-  return answer
+func choose(reader *bufio.Reader, query string, options []string) string {
+	fmt.Print(query)
+	if len(options) == 0 {
+		panic(fmt.Errorf("no options found\n"))
+	}
+	for i, option := range options {
+		fmt.Printf("%v. %v\n", i+1, option)
+	}
+	answer, err := reader.ReadString('\n')
+	answer = strings.TrimSpace(answer)
+	if err != nil {
+		panic(err)
+	}
+	chosenNumber, err := strconv.Atoi(answer)
+	if err != nil || !(0 <= chosenNumber-1 && chosenNumber-1 < len(options)) {
+		fmt.Printf("expect a number between 1 and %v\n", len(options))
+		return choose(reader, query, options)
+	}
+	return options[chosenNumber-1]
 }
 
 func checkForService(serviceRole string, existingServices []string) {
 	if contains(existingServices, serviceRole) {
-		fmt.Println("Service %s already exists in this application", serviceRole)
+		fmt.Printf("Service %v already exists in this application\n", serviceRole)
 		os.Exit(1)
 	}
 }
 
-func getExistingServices(services []string) {
+func getExistingServices(services map[string]map[string]Service) []string {
 	existingServices := []string{}
-	for protectionLevel, serviceConfigs := range services {
+	for _, serviceConfigs := range services {
 		for service := range serviceConfigs {
-			append(existingServices, service)
+			existingServices = append(existingServices, service)
 		}
 	}
+	return existingServices
 }
 
 func contains(strings []string, targetString string) bool {
