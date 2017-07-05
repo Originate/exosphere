9b3bd9aceac6768afc75bf47b7b112f79c84de12
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-07-05 14:28:45 -0700

    go: move to single binary (#305)

---
+++ exo-go/src/cmd/create.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Originate/exosphere/exo-go/src/helpers"
+	"github.com/spf13/cobra"
+	"github.com/tmrts/boilr/pkg/template"
+)
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create a new Exosphere application",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 && args[0] == "help" {
+			err := cmd.Help()
+			if err != nil {
+				panic(err)
+			}
+			return
+		}
+		fmt.Print("We are about to create a new Exosphere application\n\n")
+		templatePath, err := helpers.CreateTemplateDir()
+		if err != nil {
+			log.Fatalf("Failed to create the template: %s", err)
+		}
+		template, err := template.Get(templatePath)
+		if err != nil {
+			log.Fatalf("Failed to fetch the application template: %s", err)
+		}
+		if err = template.Execute("."); err != nil {
+			log.Fatalf("Failed to create the application: %s", err)
+		}
+		if err = helpers.RemoveTemplateDir(); err != nil {
+			log.Fatalf("Failed to remove the template: %s", err)
+		}
+		fmt.Println("\ndone")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(createCmd)
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/Originate/exosphere/cmd"
-
-func main() {
-	cmd.Execute()
-}
