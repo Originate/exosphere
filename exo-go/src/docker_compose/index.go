25f3c81ccc5bf5e60538a4a91aab7c7e268f57b4
Cheahuychou Mao <cmao18@mit.edu>
2017-07-11 20:07:59 -0700

    add docker compose struct and helper functions

---
+++ exo-go/src/docker_compose/index.go
@@ -0,0 +1,43 @@
+package dockerCompose
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+// RunAllImages runs all the docker images
+func RunAllImages(env []string, cwd string, write func(string)) (*exec.Cmd, *bytes.Buffer, error) {
+	return start(cwd, env, write, "docker-compose", "up")
+}
+
+// KillAllContainers kills all the containers
+func KillAllContainers(env []string, cwd string, write func(string)) (*exec.Cmd, *bytes.Buffer, error) {
+	return start(cwd, env, write, "docker-compose", "down")
+}
+
+func start(dir string, env []string, write func(string), command ...string) (*exec.Cmd, *bytes.Buffer, error) {
+	cmd := exec.Command(command[0], command[1:]...) // nolint gas
+	cmd.Dir = dir
+	if len(env) > 0 {
+		cmd.Env = env
+	}
+	var out bytes.Buffer
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return cmd, &out, err
+	}
+	scanner := bufio.NewScanner(stdout)
+	go func() {
+		for scanner.Scan() {
+			text := scanner.Text()
+			write(text)
+			out.Write([]byte(text))
+		}
+	}()
+	if err = cmd.Start(); err != nil {
+		return cmd, &out, fmt.Errorf("Error running %s\nError:%s", command, err)
+	}
+	return cmd, &out, nil
+}
---
+++ exo-go/src/docker_helpers/index.go
@@ -0,0 +1,78 @@
+package dockerHelpers
+
+import (
+	"context"
+
+	"github.com/Originate/exosphere/exo-go/src/process_helpers"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+)
+
+// RemoveDanglingImages removes all dangling images on the machine
+func RemoveDanglingImages(c *client.Client) error {
+	ctx := context.Background()
+	filtersArgs := filters.NewArgs()
+	filtersArgs.Add("dangling", "true")
+	imageSummaries, err := c.ImageList(ctx, types.ImageListOptions{
+		All:     false,
+		Filters: filtersArgs,
+	})
+	if err != nil {
+		return err
+	}
+	for _, imageSummary := range imageSummaries {
+		_, err = c.ImageRemove(ctx, imageSummary.ID, types.ImageRemoveOptions{})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RemoveDanglingVolumes removes all dangling volumes on the machine
+func RemoveDanglingVolumes(c *client.Client) error {
+	ctx := context.Background()
+	filtersArgs := filters.NewArgs()
+	filtersArgs.Add("dangling", "true")
+	volumesListOKBody, err := c.VolumeList(ctx, filtersArgs)
+	if err != nil {
+		return err
+	}
+	for _, volume := range volumesListOKBody.Volumes {
+		err = c.VolumeRemove(ctx, volume.Name, false)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// PullImage pulls the given image from DockerHub
+func PullImage(c *client.Client, image string) error {
+	ctx := context.Background()
+	_, err := c.ImagePull(ctx, image, types.ImagePullOptions{})
+	return err
+}
+
+// CatFile reads the file fileName inside the docker image image
+func CatFile(c *client.Client, image, fileName string, done func(err error, content []byte)) {
+	if err := PullImage(c, image); err != nil {
+		done(err, []byte(""))
+	} else {
+		output, err := processHelpers.Run("", "docker", "run", image, "cat", fileName)
+		done(err, []byte(output))
+	}
+}
+
+// ListRunningContainers passes a slice of the names of running containers
+// and error (if any) to the callback function
+func ListRunningContainers(c *client.Client, done func([]string, error)) {
+	containerNames := []string{}
+	ctx := context.Background()
+	containers, err := c.ContainerList(ctx, types.ContainerListOptions{})
+	for _, container := range containers {
+		containerNames = append(containerNames, container.Image)
+	}
+	done(containerNames, err)
+}
