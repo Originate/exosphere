bd0b556bf7440d279ad48066774d8aed6b5d0df9
Cheahuychou Mao <cmao18@mit.edu>
2017-07-13 01:22:06 -0700

    update docker-compose struct and remove unrelated changes

--- exo-go/src/types/docker_compose.go
+++ exo-go/src/types/docker_compose.go
@@ -6,29 +6,16 @@ type DockerCompose struct {
 	Services map[string]Service
 }
 
-// Service represents the service object in DockerCompose object
+// Service represents the configuration of a service as provided in
+// docker-compose.yml
 type Service struct {
-	Image         string      `yaml:",omitempty"`
-	Build         string      `yaml:",omitempty"`
-	Command       string      `yaml:",omitempty"`
-	ContainerName string      `yaml:",omitempty"`
-	Ports         []string    `yaml:",omitempty"`
-	Volumes       []string    `yaml:",omitempty"`
-	Links         []string    `yaml:",omitempty"`
-	Environment   Environment `yaml:",omitempty"`
-	DependsOn     []string    `yaml:",omitempty"`
-}
-
-// Environment represents the environment variables of the service
-type Environment struct {
-	ServiceRoutes []ServiceRoute `yaml:"SERVICE_ROUTES,omitempty"`
-}
-
-// ServiceRoute represents the route that the service uses to communicate
-// with exocom
-type ServiceRoute struct {
-	Role      string   `yaml:",omitempty"`
-	Receives  []string `yaml:",omitempty"`
-	Sends     []string `yaml:",omitempty"`
-	Namespace string   `yaml:",omitempty"`
+	Image         string            `yaml:",omitempty"`
+	Build         string            `yaml:",omitempty"`
+	Command       string            `yaml:",omitempty"`
+	ContainerName string            `yaml:",omitempty"`
+	Ports         []string          `yaml:",omitempty"`
+	Volumes       []string          `yaml:",omitempty"`
+	Links         []string          `yaml:",omitempty"`
+	Environment   map[string]string `yaml:",omitempty"`
+	DependsOn     []string          `yaml:",omitempty"`
 }
