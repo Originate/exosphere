6c6a8148e04e0bde33a2c8b20e0e1e07b70ae129
Cheahuychou Mao <cmao18@mit.edu>
2017-07-13 11:05:53 -0700

    add tests for ParseDockerComposeLog

--- exo-go/src/util/index_suite_test.go
+++ exo-go/src/util/index_suite_test.go
@@ -1,4 +1,4 @@
-package logger_test
+package util_test
 
 import (
 	"testing"
@@ -9,5 +9,5 @@ import (
 
 func TestLogger(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Logger Suite")
+	RunSpecs(t, "ParseDockerComposeLog Suite")
 }
