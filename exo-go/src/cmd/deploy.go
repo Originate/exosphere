22313b9e0e99e9a93d8c8ec8931620bb1d953710
Hugo Ho <hugobho@users.noreply.github.com>
2017-07-14 11:17:44 -0700

    update

--- exo-go/src/cmd/deploy.go
+++ exo-go/src/cmd/deploy.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/Originate/exosphere/exo-go/src/app_config_helpers"
 	"github.com/Originate/exosphere/exo-go/src/terraform_file_helpers"
-	"github.com/Originate/exosphere/exo-go/src/util"
 	"github.com/spf13/cobra"
 )
 
@@ -15,13 +14,17 @@ var deployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "Deploys Exosphere application to the cloud",
 	Run: func(cmd *cobra.Command, args []string) {
-		if util.PrintHelpIfNecessary(cmd, args) {
+		if printHelpIfNecessary(cmd, args) {
 			return
 		}
 		fmt.Println("We are about to deploy an application!")
 
-		appConfig := appConfigHelpers.GetAppConfig()
-		err := terraformFileHelpers.GenerateTerraform(appConfig)
+		appConfig, err := appConfigHelpers.GetAppConfig()
+		if err != nil {
+			log.Fatalf("Failed to read application configuration: %s", err)
+		}
+
+		err = terraformFileHelpers.GenerateTerraform(appConfig)
 		if err != nil {
 			log.Fatalf("Deploy failed: %s", err)
 		}
