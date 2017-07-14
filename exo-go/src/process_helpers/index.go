ee22f31a6c6f68ce95dea08dc38fd32a21924412
Cheahuychou Mao <cmao18@mit.edu>
2017-07-13 17:10:00 -0700

    add process struct and fix related tests

--- exo-go/src/process_helpers/index.go
+++ exo-go/src/process_helpers/index.go
@@ -1,11 +1,8 @@
 package processHelpers
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
-	"os/exec"
 
 	shellwords "github.com/mattn/go-shellwords"
 	"github.com/pkg/errors"
@@ -14,29 +11,27 @@ import (
 // Run runs the given command, waits for the process to finish and
 // returns the output string and error (if any)
 func Run(dir string, commandWords ...string) (string, error) {
-	cmd := exec.Command(commandWords[0], commandWords[1:]...) // nolint gass
-	cmd.Dir = dir
-	outputArray, err := cmd.CombinedOutput()
-	output := string(outputArray)
-	return output, err
+	process := NewProcess(commandWords...)
+	process.SetDir(dir)
+	return process.Run()
 }
 
-// Start runs the given command in the given dir directory, and returns
-// the pointer to the command, stdout pipe, output buffer and error (if any)
-func Start(dir string, commandWords ...string) (*exec.Cmd, io.WriteCloser, *bytes.Buffer, error) {
-	cmd := exec.Command(commandWords[0], commandWords[1:]...) // nolint gas
-	cmd.Dir = dir
-	in, err := cmd.StdinPipe()
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	if err != nil {
-		return nil, in, &out, err
-	}
-	if err = cmd.Start(); err != nil {
-		return nil, in, &out, fmt.Errorf("Error running %s\nError:%s", commandWords, err)
-	}
-	return cmd, in, &out, nil
-}
+// // Start runs the given command in the given dir directory, and returns
+// // the pointer to the command, stdout pipe, output buffer and error (if any)
+// func Start(dir string, commandWords ...string) (*exec.Cmd, io.WriteCloser, *bytes.Buffer, error) {
+// 	cmd := exec.Command(commandWords[0], commandWords[1:]...) // nolint gas
+// 	cmd.Dir = dir
+// 	in, err := cmd.StdinPipe()
+// 	var out bytes.Buffer
+// 	cmd.Stdout = &out
+// 	if err != nil {
+// 		return nil, in, &out, err
+// 	}
+// 	if err = cmd.Start(); err != nil {
+// 		return nil, in, &out, fmt.Errorf("Error running %s\nError:%s", commandWords, err)
+// 	}
+// 	return cmd, in, &out, nil
+// }
 
 // RunSeries runs each command in commands and returns an error if any
 func RunSeries(dir string, commands [][]string) error {
---
+++ exo-go/src/process_helpers/process.go
@@ -0,0 +1,108 @@
+package processHelpers
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+	"strings"
+
+	"github.com/Originate/exosphere/exo-go/src/util"
+)
+
+// Process represents a exec.Cmd process
+type Process struct {
+	Cmd        *exec.Cmd
+	StdoutLog  func(string)
+	StdoutPipe io.ReadCloser
+	StdinPipe  io.WriteCloser
+	Output     string
+}
+
+// NewProcess is Process's constructor
+func NewProcess(commandWords ...string) *Process {
+	process := &Process{Cmd: exec.Command(commandWords[0], commandWords[1:]...)} //nolint gas
+	return process
+}
+
+// Run runs the process, waits for the process to finish and
+// returns the output string and error (if any)
+func (process *Process) Run() (string, error) {
+	outputArray, err := process.Cmd.CombinedOutput()
+	process.Output = string(outputArray)
+	return process.Output, err
+}
+
+// SetDir sets the directory that the process should be run in
+func (process *Process) SetDir(dir string) {
+	process.Cmd.Dir = dir
+}
+
+// SetEnv sets the environment for the process
+func (process *Process) SetEnv(env []string) {
+	process.Cmd.Env = env
+}
+
+// SetStdoutLog sets the function that process should use to log
+// the stdout output
+func (process *Process) SetStdoutLog(log func(string)) {
+	process.StdoutLog = log
+}
+
+// Start runs the process and returns an error if any
+func (process *Process) Start() error {
+	var err error
+	process.StdinPipe, err = process.Cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	process.StdoutPipe, err = process.Cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	// go func() {
+	// 	var buf bytes.Buffer
+	// 	tee := io.TeeReader(process.StdoutPipe, &buf)
+	// 	scanner := bufio.NewScanner(tee)
+	// 	for scanner.Scan() {
+	// 		text := scanner.Text()
+	// 		if process.StdoutLog != nil {
+	// 			process.StdoutLog(text)
+	// 		}
+	// 		process.Output = process.Output + text
+	// 	}
+	// }()
+	return process.Cmd.Start()
+}
+
+// WaitForText reads from the StdoutPipe stream waiting for the given text
+// for the given duration and updates Output with the output it reads while
+// waiting
+func (process *Process) WaitForText(text string, duration int) error {
+	var err error
+	var output string
+	return util.WaitForf(func() bool {
+		buffer := make([]byte, 1000)
+		var count int
+		count, err = process.StdoutPipe.Read(buffer)
+		if count == 0 {
+			if err == io.EOF {
+				return false
+			}
+			if err != nil {
+				return false
+			}
+		}
+		output = output + string(buffer)
+		return strings.Contains(output, text)
+	}, func() error {
+		if err != nil && err != io.EOF {
+			return err
+		}
+		return fmt.Errorf("Expected '%s' to include '%s'", process.Output, text)
+	}, duration)
+}
+
+// Wait waits for the process to finish, can only be called after Start()
+func (process *Process) Wait() error {
+	return process.Cmd.Wait() // TODO: update process.Output
+}
