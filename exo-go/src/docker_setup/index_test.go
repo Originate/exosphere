1327e9b56845f2688e335979ebf43c99d9bcc193
Cheahuychou Mao <cmao18@mit.edu>
2017-07-19 14:52:33 -0700

    write more tests

--- exo-go/src/docker_setup/index_test.go
+++ exo-go/src/docker_setup/index_test.go
@@ -1,18 +1,60 @@
 package dockerSetup_test
 
-// import (
-// 	. "github.com/onsi/ginkgo"
-// 	. "github.com/onsi/gomega"
-// )
+import (
+	"path"
 
-// var _ = Describe("GetServiceDockerConfigs", func() {
+	"github.com/Originate/exosphere/exo-go/src/app_config_helpers"
+	"github.com/Originate/exosphere/exo-go/src/docker_setup"
+	"github.com/Originate/exosphere/exo-go/src/logger"
+	"github.com/Originate/exosphere/exo-go/src/service_config_helpers"
+	"github.com/Originate/exosphere/exo-go/src/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
 
-// 	var dockerSetup dockerSetup.DockerSetup
-// 	var appConfig types.AppConfig
-// 	var appDir string
+var _ = Describe("GetServiceDockerConfigs", func() {
 
-// 	var _ = BeforeSuite(func() {
-// 		appConfig = appConfigHelpers.GetAppConfig(appDir)
-// 	})
+	var setup *dockerSetup.DockerSetup
+	var appDir string
 
-// })
+	var _ = BeforeSuite(func() {
+		appDir = path.Join("..", "..", "..", "exosphere-shared", "example-apps", "external-dependency")
+		appConfig, err := appConfigHelpers.GetAppConfig(appDir)
+		Expect(err).NotTo(HaveOccurred())
+		serviceConfigs, err := serviceConfigHelpers.GetServiceConfigs(appDir, appConfig)
+		Expect(err).NotTo(HaveOccurred())
+		serviceData := serviceConfigHelpers.GetServiceData(appConfig.Services)
+		serviceName := "mongo"
+		setup = dockerSetup.NewDockerSetup(appConfig, serviceConfigs[serviceName], serviceData[serviceName], serviceName, logger.NewLogger([]string{}, []string{}), appDir)
+	})
+
+	var _ = Describe("GetServiceDockerConfigs", func() {
+		var dockerConfigs map[string]types.DockerConfig
+
+		It("should get docker configs sucessfully", func() {
+			var err error
+			dockerConfigs, err = setup.GetServiceDockerConfigs()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should include the docker config for the service itself", func() {
+			dockerConfig, exists := dockerConfigs["mongo"]
+			Expect(exists).To(Equal(true))
+			Expect(dockerConfig).To(Equal(types.DockerConfig{
+				Build:         "../mongo",
+				ContainerName: "mongo",
+				Command:       "node_modules/exoservice/bin/exo-js",
+				Links:         []string{"mongo3.4.0:mongo"},
+				Environment: map[string]string{
+					"ROLE":        "mongo",
+					"EXOCOM_HOST": "exocom0.22.1",
+					"EXOCOM_PORT": "$EXOCOM_PORT",
+					"MONGO":       "mongo",
+				},
+				DependsOn: []string{"exocom0.22.1", "mongo3.4.0"},
+			}))
+		})
+
+	})
+
+})
