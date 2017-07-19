1e692178ced90d977dbdeb187b0a829df71553c3
Cheahuychou Mao <cmao18@mit.edu>
2017-07-19 13:57:39 -0700

    start writing tests

---
+++ exo-go/src/docker_setup/index_test.go
@@ -0,0 +1,18 @@
+package dockerSetup_test
+
+// import (
+// 	. "github.com/onsi/ginkgo"
+// 	. "github.com/onsi/gomega"
+// )
+
+// var _ = Describe("GetServiceDockerConfigs", func() {
+
+// 	var dockerSetup dockerSetup.DockerSetup
+// 	var appConfig types.AppConfig
+// 	var appDir string
+
+// 	var _ = BeforeSuite(func() {
+// 		appConfig = appConfigHelpers.GetAppConfig(appDir)
+// 	})
+
+// })
