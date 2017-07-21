9a0915ff73796bb4424169b7919e15d83626d154
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-07-21 08:29:01 -0700

    simplify struct methods (#355)

--- exo-go/src/app_dependency_helpers/generic.go
+++ exo-go/src/app_dependency_helpers/generic.go
@@ -15,44 +15,44 @@ type genericDependency struct {
 }
 
 // GetContainerName returns the container name
-func (dependency genericDependency) GetContainerName() string {
-	return dependency.config.Name + dependency.config.Version
+func (g *genericDependency) GetContainerName() string {
+	return g.config.Name + g.config.Version
 }
 
 //GetDeploymentConfig returns configuration needed in deployment
-func (dependency genericDependency) GetDeploymentConfig() map[string]string {
+func (g *genericDependency) GetDeploymentConfig() map[string]string {
 	config := map[string]string{
-		"version": dependency.config.Version,
+		"version": g.config.Version,
 	}
 	return config
 }
 
 // GetDockerConfig returns docker configuration and an error if any
-func (dependency genericDependency) GetDockerConfig() (types.DockerConfig, error) {
-	renderedVolumes, err := dockerHelpers.GetRenderedVolumes(dependency.config.Config.Volumes, dependency.appConfig.Name, dependency.config.Name, dependency.homeDir)
+func (g *genericDependency) GetDockerConfig() (types.DockerConfig, error) {
+	renderedVolumes, err := dockerHelpers.GetRenderedVolumes(g.config.Config.Volumes, g.appConfig.Name, g.config.Name, g.homeDir)
 	if err != nil {
 		return types.DockerConfig{}, err
 	}
 	return types.DockerConfig{
-		Image:         fmt.Sprintf("%s:%s", dependency.config.Name, dependency.config.Version),
-		ContainerName: dependency.GetContainerName(),
-		Ports:         dependency.config.Config.Ports,
+		Image:         fmt.Sprintf("%s:%s", g.config.Name, g.config.Version),
+		ContainerName: g.GetContainerName(),
+		Ports:         g.config.Config.Ports,
 		Volumes:       renderedVolumes,
 	}, nil
 }
 
 // GetEnvVariables returns the environment variables
-func (dependency genericDependency) GetEnvVariables() map[string]string {
-	return dependency.config.Config.DependencyEnvironment
+func (g *genericDependency) GetEnvVariables() map[string]string {
+	return g.config.Config.DependencyEnvironment
 }
 
 // GetOnlineText returns the online text
-func (dependency genericDependency) GetOnlineText() string {
-	return dependency.config.Config.OnlineText
+func (g *genericDependency) GetOnlineText() string {
+	return g.config.Config.OnlineText
 }
 
 // GetServiceEnvVariables returns the environment variables that need to
 // be passed to services that use it
-func (dependency genericDependency) GetServiceEnvVariables() map[string]string {
-	return dependency.config.Config.ServiceEnvironment
+func (g *genericDependency) GetServiceEnvVariables() map[string]string {
+	return g.config.Config.ServiceEnvironment
 }
