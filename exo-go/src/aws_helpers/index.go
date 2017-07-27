7198bbee1e98062b64eb9b854217f096de8092d1
Hugo Ho <hugobho@users.noreply.github.com>
2017-07-27 10:22:51 -0700

    extract aws/terraform helper

--- exo-go/src/aws_helpers/index.go
+++ exo-go/src/aws_helpers/index.go
@@ -1,103 +1,34 @@
 package awsHelper
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-// Configure sets up a blank AWS account to have s3 bucket and dynamodb table
-func Configure(bucketName, tableName, region string) error {
-	config := aws.NewConfig().WithRegion(region)
-	session := session.Must(session.NewSession())
-
-	err := createRemoteState(session, config, bucketName)
-	if err != nil {
-		return err
-	}
-
-	return createLockTable(session, config, tableName)
-}
-
-// creates s3 bucket to store terraform remote state
-func createRemoteState(currSession *session.Session, config *aws.Config, bucketName string) error {
-	s3client := s3.New(currSession, config)
-
+func hasBucket(s3client *s3.S3, bucketName string) (bool, error) {
 	buckets, err := s3client.ListBuckets(&s3.ListBucketsInput{})
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	if !hasBucket(buckets.Buckets, bucketName) {
-		_, err = s3client.CreateBucket(&s3.CreateBucketInput{Bucket: &bucketName})
-		if err != nil {
-			return err
+	for _, bucket := range buckets.Buckets {
+		if bucketName == *bucket.Name {
+			return true, err
 		}
-
-		// enable versioning
-		versioningStatus := "Enabled"
-		_, err = s3client.PutBucketVersioning(&s3.PutBucketVersioningInput{
-			Bucket: &bucketName,
-			VersioningConfiguration: &s3.VersioningConfiguration{
-				Status: &versioningStatus,
-			},
-		})
 	}
-
-	return err
+	return false, err
 }
 
-// creates dynamodb table to store terraform lock file
-func createLockTable(currSession *session.Session, config *aws.Config, tableName string) error {
-	dynamodbClient := dynamodb.New(currSession, config)
-
+func hasTable(dynamodbClient *dynamodb.DynamoDB, tableName string) (bool, error) {
 	tables, err := dynamodbClient.ListTables(&dynamodb.ListTablesInput{})
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	if !hasTable(tables.TableNames, tableName) {
-		input := &dynamodb.CreateTableInput{
-			AttributeDefinitions: []*dynamodb.AttributeDefinition{
-				{
-					AttributeName: aws.String("LockID"),
-					AttributeType: aws.String("S"),
-				},
-			},
-			KeySchema: []*dynamodb.KeySchemaElement{
-				{
-					AttributeName: aws.String("LockID"),
-					KeyType:       aws.String("HASH"),
-				},
-			},
-			TableName: aws.String(tableName),
-			ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-				ReadCapacityUnits:  aws.Int64(5),
-				WriteCapacityUnits: aws.Int64(5),
-			},
-		}
-
-		_, err = dynamodbClient.CreateTable(input)
-	}
-
-	return err
-}
-
-func hasBucket(buckets []*s3.Bucket, bucketName string) bool {
-	for _, bucket := range buckets {
-		if bucketName == *bucket.Name {
-			return true
-		}
-	}
-	return false
-}
-
-func hasTable(tables []*string, tableName string) bool {
-	for _, table := range tables {
+	for _, table := range tables.TableNames {
 		if tableName == *table {
-			return true
+			return true, err
 		}
 	}
-	return false
+	return false, err
 }
