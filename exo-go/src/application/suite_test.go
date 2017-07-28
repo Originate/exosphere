1da2f6f78d0abc6f29ce83ca04333bc7da33d61f
Charles Rudolph <charles.rudolph@originate.com>
2017-07-28 15:58:10 -0700

    update

--- exo-go/src/application/suite_test.go
+++ exo-go/src/application/suite_test.go
@@ -1,4 +1,4 @@
-package appSetup_test
+package application_test
 
 import (
 	"testing"
@@ -9,5 +9,5 @@ import (
 
 func TestAppSetup(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "AppSetup Suite")
+	RunSpecs(t, "Application Suite")
 }
