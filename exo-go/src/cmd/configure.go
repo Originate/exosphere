90b3302076fcf90d1af511c3b0b62d4ab0ec8d66
Hugo Ho <hugobho@users.noreply.github.com>
2017-08-03 11:52:57 -0700

    fix tests

--- exo-go/src/cmd/configure.go
+++ exo-go/src/cmd/configure.go
@@ -77,7 +77,7 @@ var configureCreateCmd = &cobra.Command{
 		existingSecrets := types.NewSecrets(secretsString)
 
 		newSecrets := map[string]string{}
-		for true {
+		for {
 			secretName := prompt.String("Secret name (leave blank to finish prompting)")
 			if secretName == "" {
 				break
