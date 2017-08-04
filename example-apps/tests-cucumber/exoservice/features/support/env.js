2c2fdc611ec0c682cfed41b135c4f5a6af9f0593
Cheahuychou Mao <cmao18@mit.edu>
2017-08-04 15:46:17 -0700

    remove World from env.js

--- example-apps/tests-cucumber/exoservice/features/support/env.js
+++ example-apps/tests-cucumber/exoservice/features/support/env.js
@@ -4,7 +4,6 @@ fs = require('fs-extra')
 defineSupportCode(function({After, setDefaultTimeout, setWorldConstructor}) {
 
   setDefaultTimeout(1000)
-  setWorldConstructor(World)
   After(function () {
   	this.exocom && this.exocom.close()
   	this.process && this.process.close()
