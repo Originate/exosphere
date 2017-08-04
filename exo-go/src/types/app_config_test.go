03151e0bac864d75878afa3f3fe07b2fd469a14e
Charles Rudolph <charles.rudolph@originate.com>
2017-08-03 18:50:54 -0700

    update

--- exo-go/src/types/app_config_test.go
+++ exo-go/src/types/app_config_test.go
@@ -29,7 +29,7 @@ var _ = Describe("AppConfig", func() {
 			Expect(appConfig.Dependencies).To(Equal([]types.Dependency{
 				types.Dependency{
 					Name:    "exocom",
-					Version: "0.22.1",
+					Version: "0.23.0",
 				},
 				types.Dependency{
 					Name:    "mongo",
