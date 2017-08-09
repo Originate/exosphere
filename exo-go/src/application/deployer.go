9d11dd9bf6689f77d10cbadb23e019c5cb268f58
Hugo Ho <hugobho@users.noreply.github.com>
2017-08-09 11:52:09 -0700

    add prints

--- exo-go/src/application/deployer.go
+++ exo-go/src/application/deployer.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"github.com/Originate/exosphere/exo-go/src/aws"
@@ -11,21 +12,25 @@ import (
 // StartDeploy starts the deployment process
 func StartDeploy(deployConfig types.DeployConfig) error {
 	terraformDir := filepath.Join(deployConfig.AppDir, "terraform")
+	fmt.Printf("Setting up AWS account...\n\n")
 	err := aws.InitAccount(deployConfig.AwsConfig)
 	if err != nil {
 		return err
 	}
 
+	fmt.Printf("\n\nGenerating Terraform files...\n\n")
 	err = terraform.GenerateFile(deployConfig, terraformDir)
 	if err != nil {
 		return err
 	}
 
+	fmt.Printf("\n\nRetrieving remote state...\n\n")
 	err = terraform.RunInit(terraformDir, deployConfig.Logger)
 	if err != nil {
 		return err
 	}
 
 	secretsPath := filepath.Join(terraformDir, "secrets.tfvars")
+	fmt.Printf("\n\nPlanning deployment...\n\n")
 	return terraform.RunPlan(terraformDir, secretsPath, deployConfig.Logger)
 }
