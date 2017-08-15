b1394427a970f928f39f49b76a59e1c4ca8cb248
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-08-15 08:24:39 -0700

    update

--- test_helpers/clean_feature_context.go
+++ test_helpers/clean_feature_context.go
@@ -32,7 +32,7 @@ func CleanFeatureContext(s *godog.Suite) {
 
 	s.Step(`^my machine has both dangling and non-dangling Docker images and volumes$`, func() error {
 		appName := "external-dependency"
-		serviceName := "mongo"
+		serviceName := "mongo-service"
 		err := CheckoutApp(cwd, appName)
 		if err != nil {
 			return fmt.Errorf("Error checking out app: %v", err)
