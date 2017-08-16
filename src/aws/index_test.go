3931e1a633631b739b6b2748ff509f7ce411319b
Hugo <hugo.ho@originate.com>
2017-08-16 10:15:06 -0700

    address comments

--- src/aws/index_test.go
+++ src/aws/index_test.go
@@ -1,13 +1,7 @@
 package aws_test
 
 import (
-	"os"
-	"path"
-
-	"github.com/Originate/exosphere/src/aws"
 	"github.com/Originate/exosphere/src/types"
-	"github.com/Originate/exosphere/src/util"
-	"github.com/Originate/exosphere/test_helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -53,48 +47,5 @@ var4="val4"`
 		actualSecrets := types.NewSecrets(existingTfVars).Merge(newSecrets)
 
 		Expect(expectedTfString).To(Equal(actualSecrets.TfString()))
-	})
-})
-
-var _ = Describe("ECR helpers", func() {
-	It("compiles the list of image names", func() {
-		cwd, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-		err = testHelpers.CheckoutApp(cwd, "test")
-		Expect(err).NotTo(HaveOccurred())
-		appDir := path.Join("tmp", "test")
-		homeDir, err := util.GetHomeDirectory()
-		if err != nil {
-			panic(err)
-		}
-		appConfig, err := types.NewAppConfig(appDir)
-		Expect(err).NotTo(HaveOccurred())
-
-		dockerCompose := types.DockerCompose{
-			Services: map[string]types.DockerConfig{
-				"web":       types.DockerConfig{},
-				"users":     types.DockerConfig{},
-				"dashboard": types.DockerConfig{},
-			},
-		}
-		deployConfig := types.DeployConfig{
-			AppDir:    appDir,
-			HomeDir:   homeDir,
-			AppConfig: appConfig,
-		}
-		imageNames, err := aws.GetImageNames(deployConfig, "./tmp", dockerCompose)
-		Expect(err).NotTo(HaveOccurred())
-		expectedImages := map[string]string{
-			"exocom":    "originate/exocom:0.24.0",
-			"users":     "tmp_users",
-			"dashboard": "tmp_dashboard",
-			"web":       "tmp_web",
-		}
-
-		for k, v := range expectedImages {
-			Expect(imageNames[k]).To(Equal(v))
-		}
 	})
 })
