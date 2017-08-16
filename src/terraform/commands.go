eb7c3d955506f069b3ddbe89215dfaa492c4d56c
Hugo <hugo.ho@originate.com>
2017-08-16 14:52:57 -0700

    fix

--- src/terraform/commands.go
+++ src/terraform/commands.go
@@ -10,8 +10,8 @@ import (
 
 // RunInit runs the 'terraform init' command and force copies the remote state
 func RunInit(deployConfig types.DeployConfig) error {
-	if util.DoesFileExist(deployConfig.TerraformFile) {
-		return errors.New("terraform initialization attempted in empty directory. Please make sure 'exo deploy build' has been run")
+	if !util.DoesFileExist(deployConfig.TerraformFile) {
+		return errors.New("terraform initialization attempted in directory with no 'main.tf' file. Please make sure 'exo deploy build' has been run")
 	}
 	err := util.RunAndLog(deployConfig.TerraformDir, []string{}, deployConfig.LogChannel, "terraform", "init", "-force-copy")
 	if err != nil {
