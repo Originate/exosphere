7c6eaf0b6963ae9bc16b4bcf125b027d385a4b10
Charles Rudolph <charles.rudolph@originate.com>
2017-08-17 15:51:43 -0700

    update

--- src/application/service_tester.go
+++ src/application/service_tester.go
@@ -131,8 +131,5 @@ func (s *ServiceTester) Run() (bool, error) {
 	if err := s.Runner.Logger.Log("exo-test", fmt.Sprintf("'%s' tests %s", s.Role, resultString), true); err != nil {
 		return testPassed, err
 	}
-	if err := s.Shutdown(types.ShutdownConfig{CloseMessage: "killing test containers\n"}); err != nil {
-		return testPassed, err
-	}
-	return testPassed, nil
+	return testPassed, s.Shutdown(types.ShutdownConfig{CloseMessage: "killing test containers\n"})
 }
