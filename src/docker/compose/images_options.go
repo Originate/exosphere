135901e38730b023fd838200b2f3bf8ff31d99bc
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-08-23 11:17:14 -0700

    Update images_options.go

--- src/docker/compose/images_options.go
+++ src/docker/compose/images_options.go
@@ -1,6 +1,6 @@
 package compose
 
-// ImagesOptions is the options to compose functions that deal with multiple image
+// ImagesOptions is the options to compose functions that deal with multiple images
 type ImagesOptions struct {
 	DockerComposeDir string
 	Env              []string
