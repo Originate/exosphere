b7f1fdf9f5e52e5dc8f55d4bc1e5976ab314b049
Hugo Ho <hugobho@users.noreply.github.com>
2017-08-23 16:25:56 -0700

    switch secrets to json format
    closes #474

--- src/aws/secrets_helpers.go
+++ src/aws/secrets_helpers.go
@@ -2,44 +2,52 @@ package aws
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
+	"strings"
 
 	"github.com/Originate/exosphere/src/types"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/pkg/errors"
 )
 
-const secretsFile string = "secrets.tfvars"
+const secretsFile string = "secrets.json"
 
 // CreateSecretsStore creates an S3 bucket  and file object used for secrets management
 func CreateSecretsStore(awsConfig types.AwsConfig) error {
 	s3client := createS3client(awsConfig.Region)
-	return createS3Object(s3client, nil, awsConfig.SecretsBucket, secretsFile)
+	return createS3Object(s3client, strings.NewReader("{}"), awsConfig.SecretsBucket, secretsFile)
 }
 
 // ReadSecrets reads secret key value pair from remote store
-func ReadSecrets(awsConfig types.AwsConfig) (string, error) {
+func ReadSecrets(awsConfig types.AwsConfig) (types.Secrets, error) {
 	s3client := createS3client(awsConfig.Region)
-	err := createS3Object(s3client, nil, awsConfig.SecretsBucket, secretsFile)
+	err := createS3Object(s3client, strings.NewReader("{}"), awsConfig.SecretsBucket, secretsFile)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	results, err := s3client.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(awsConfig.SecretsBucket),
 		Key:    aws.String(secretsFile),
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	objectBytes, err := ioutil.ReadAll(results.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	err = results.Body.Close()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return string(objectBytes), err
+	secrets := types.Secrets{}
+	err = json.Unmarshal(objectBytes, &secrets)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot unmarshal secrets into map")
+	}
+	return secrets, nil
 }
 
 // MergeAndWriteSecrets merges two secret maps and writes them to s3
@@ -51,16 +59,20 @@ func MergeAndWriteSecrets(existingSecrets, newSecrets types.Secrets, awsConfig t
 
 // DeleteSecrets deletes a list of secrets provided their keys. Ignores them if they don't exist
 func DeleteSecrets(secretKeys []string, awsConfig types.AwsConfig) error {
-	tfvars, err := ReadSecrets(awsConfig)
+	secrets, err := ReadSecrets(awsConfig)
 	if err != nil {
 		return err
 	}
-	secrets := types.NewSecrets(tfvars).Delete(secretKeys)
-	return writeSecrets(secrets, awsConfig)
+	newSecrets := secrets.Delete(secretKeys)
+	return writeSecrets(newSecrets, awsConfig)
 }
 
 func writeSecrets(secrets types.Secrets, awsConfig types.AwsConfig) error {
 	s3client := createS3client(awsConfig.Region)
-	fileBytes := bytes.NewReader([]byte(secrets.TfString()))
+	secretsString, err := json.Marshal(secrets)
+	if err != nil {
+		return errors.Wrap(err, "cannot marshal secrets map into JSON string")
+	}
+	fileBytes := bytes.NewReader([]byte(secretsString))
 	return putS3Object(s3client, fileBytes, awsConfig.SecretsBucket, secretsFile)
 }
