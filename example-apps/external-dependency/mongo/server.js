b1f7741ff7662ab716d302671d50889e31f79c67
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-09-06 18:22:32 -0700

    update

--- example-apps/external-dependency/mongo/server.js
+++ example-apps/external-dependency/mongo/server.js
@@ -3,7 +3,7 @@ const {MongoClient} = require('mongodb')
 const N = require('nitroglycerin')
 
 bootstrap({
-  'before-all': function (done) {
+  beforeAll: function (done) {
     MongoClient.connect(getMongoAddress(), N(function() {
       console.log("MongoDB connected")
       done()
