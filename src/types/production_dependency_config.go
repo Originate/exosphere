badefeeecbe458b15f0271818400a5afa9c08555
Hugo Ho <hugobho@users.noreply.github.com>
2017-09-20 14:01:16 -0700

    remove prod silenced field

--- src/types/production_dependency_config.go
+++ src/types/production_dependency_config.go
@@ -3,7 +3,6 @@ package types
 // ProductionDependencyConfig represents a production dependency
 type ProductionDependencyConfig struct {
 	Config  ProductionDependencyConfigOptions `yaml:",omitempty"`
-	Silent  bool                              `yaml:",omitempty"`
 	Name    string
 	Version string
 }
