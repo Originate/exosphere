83dc2ab92add7c2b2dd02877987f6846b7002f9c
Hugo Ho <hugobho@users.noreply.github.com>
2017-09-26 11:39:22 -0700

    cleanup [ci skip]

--- src/types/production_dependency_config_test.go
+++ src/types/production_dependency_config_test.go
@@ -9,28 +9,17 @@ import (
 var _ = Describe("ProductionDependencyConfig", func() {
 
 	Describe("validates required production fields", func() {
-		missingConfig := types.ProductionDependencyConfig{
-			Name:    "postgres",
-			Version: "0.0.1",
-			Config: types.ProductionDependencyConfigOptions{
-				Rds: types.RdsConfig{
-					Username:       "test-user",
-					PasswordEnvVar: "TEST_PASSWORD",
-				},
-			},
-		}
-		goodConfig := types.ProductionDependencyConfig{
-			Name:    "postgres",
-			Version: "0.0.1",
-			Config: types.ProductionDependencyConfigOptions{
-				Rds: types.RdsConfig{
-					DbName:         "test",
-					Username:       "test-user",
-					PasswordEnvVar: "TEST_PASSWORD",
-				},
-			},
-		}
 		It("throws an error if production fields are missing", func() {
+			missingConfig := types.ProductionDependencyConfig{
+				Name:    "postgres",
+				Version: "0.0.1",
+				Config: types.ProductionDependencyConfigOptions{
+					Rds: types.RdsConfig{
+						Username:       "test-user",
+						PasswordEnvVar: "TEST_PASSWORD",
+					},
+				},
+			}
 			err := missingConfig.ValidateFields()
 			Expect(err).To(HaveOccurred())
 			expectedErrorString := "production dependency postgres:0.0.1 missing field(s): missing required field 'Rds.DbName'"
@@ -38,6 +27,17 @@ var _ = Describe("ProductionDependencyConfig", func() {
 		})
 
 		It("does not throw an error if production fields are not missing", func() {
+			goodConfig := types.ProductionDependencyConfig{
+				Name:    "postgres",
+				Version: "0.0.1",
+				Config: types.ProductionDependencyConfigOptions{
+					Rds: types.RdsConfig{
+						DbName:         "test",
+						Username:       "test-user",
+						PasswordEnvVar: "TEST_PASSWORD",
+					},
+				},
+			}
 			err := goodConfig.ValidateFields()
 			Expect(err).ToNot(HaveOccurred())
 		})
