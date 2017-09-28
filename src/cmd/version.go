b592bd69eb1ea260a234d0a41a9c52d8c33466f5
Hugo Ho <hugobho@users.noreply.github.com>
2017-09-28 14:37:27 -0700

    update CHANGELOG.md

--- src/cmd/version.go
+++ src/cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Displays the version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Exosphere v0.26.2")
+		fmt.Println("Exosphere v0.26.3")
 	},
 }
 
