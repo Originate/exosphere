c872f84d447f4ad46745bc32f83ee4e191197e9b
Charles Rudolph <charles.rudolph@originate.com>
2017-10-06 18:35:24 -0700

    terraform: update module hash

--- src/cmd/deploy.go
+++ src/cmd/deploy.go
@@ -62,7 +62,7 @@ var deployCmd = &cobra.Command{
 			DeployServicesOnly: deployServicesFlag,
 
 			// git commit hash of the Terraform modules in Originate/exosphere we are using
-			TerraformModulesRef: "e650982f",
+			TerraformModulesRef: "d5c193c5",
 		}
 
 		err = application.StartDeploy(deployConfig)
