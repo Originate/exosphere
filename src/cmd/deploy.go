643bf80befcac2239275af503f3cad05d62b21ae
Hugo Ho <hugobho@users.noreply.github.com>
2017-10-18 11:52:35 -0700

    release 0.28.1 (with updated terraform hash)

--- src/cmd/deploy.go
+++ src/cmd/deploy.go
@@ -62,7 +62,7 @@ var deployCmd = &cobra.Command{
 			DeployServicesOnly: deployServicesFlag,
 
 			// git commit hash of the Terraform modules in Originate/exosphere we are using
-			TerraformModulesRef: "d5c193c5",
+			TerraformModulesRef: "01891552",
 		}
 
 		err = deployer.StartDeploy(deployConfig)
