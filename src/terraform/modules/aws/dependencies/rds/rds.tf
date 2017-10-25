b285eb68ac0a8d0cf317b098f12851b29a42e915
Hugo Ho <hugobho@users.noreply.github.com>
2017-10-25 09:56:16 -0700

    address comments

--- src/terraform/modules/aws/dependencies/rds/rds.tf
+++ src/terraform/modules/aws/dependencies/rds/rds.tf
@@ -48,8 +48,8 @@ resource "aws_security_group" "rds" {
 
   ingress {
     from_port       = 5432
-    to_port         = 0
-    protocol        = -1
+    to_port         = 5432
+    protocol        = "tcp"
     security_groups = ["${var.bastion_security_group}"]
   }
 
