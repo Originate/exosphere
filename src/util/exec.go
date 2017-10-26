2bc4eab7bdf54264f0a8216589e8ef857effef03
Hugo Ho <hugobho@users.noreply.github.com>
2017-10-26 16:53:34 -0700

    remove logger

--- src/util/exec.go
+++ src/util/exec.go
@@ -29,26 +29,6 @@ func Run(dir string, commandWords ...string) (string, error) {
 	return cmdPlus.GetOutput(), nil
 }
 
-// RunAndLog runs the given command, logs the process to the given
-// channel, waits for the process to finish and returns an error (if any)
-func RunAndLog(dir string, env []string, logger *Logger, commandWords ...string) error {
-	if len(commandWords) == 1 {
-		var err error
-		commandWords, err = ParseCommand(commandWords[0])
-		if err != nil {
-			return err
-		}
-	}
-	cmdPlus := execplus.NewCmdPlus(commandWords...)
-	cmdPlus.SetDir(dir)
-	cmdPlus.AppendEnv(env)
-	ConnectLogChannel(cmdPlus, logger)
-	if err := cmdPlus.Run(); err != nil {
-		return errors.Wrapf(err, "Error running '%s'. Output:\n%s", strings.Join(commandWords, " "), cmdPlus.GetOutput())
-	}
-	return nil
-}
-
 // RunAndPipe runs the given command, and logs the process to the given writer
 func RunAndPipe(dir string, env []string, writer io.Writer, commandWords ...string) error {
 	if len(commandWords) == 1 {
@@ -84,17 +64,4 @@ func RunSeries(dir string, commands [][]string) error {
 // ParseCommand parses the command string into a string array
 func ParseCommand(command string) ([]string, error) {
 	return shellwords.Parse(command)
-}
-
-// ConnectLogChannel connects a log channel that wants to receive only each new output
-func ConnectLogChannel(cmdPlus *execplus.CmdPlus, logger *Logger) {
-	outputChannel, _ := cmdPlus.GetOutputChannel()
-	go func() {
-		for {
-			outputChunk := <-outputChannel
-			if outputChunk.Chunk != "" {
-				logger.Log(outputChunk.Chunk)
-			}
-		}
-	}()
 }
