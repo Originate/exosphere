d2614ee6f9bf65044d2cb1a3266325944b8ccb93
Hugo Ho <hugobho@users.noreply.github.com>
2017-11-07 14:55:00 -0800

    refactor docker-compose to fix exo-test build bug

--- src/docker/compose/index.go
+++ src/docker/compose/index.go
@@ -4,52 +4,36 @@ import (
 	"github.com/Originate/exosphere/src/util"
 )
 
-// BuildImage builds the given docker image
-func BuildImage(opts ImageOptions) error {
-	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "build", opts.ImageName}
-	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
-}
-
-// BuildAllImages builds all the docker images defined in docker-compose.yml
-func BuildAllImages(opts BaseOptions) error {
+// BuildImages builds images in opts.ImageNames, or builds all images if opts.ImageNames is empty
+func BuildImages(opts CommandOptions) error {
 	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "build"}
-	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
-}
-
-// KillAllContainers kills all the containers
-func KillAllContainers(opts BaseOptions) error {
-	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "down"}
-	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
-}
-
-// PullImage builds the given docker image
-func PullImage(opts ImageOptions) error {
-	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "pull", opts.ImageName}
-	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
-}
-
-// PullAllImages pulls all the docker images defined in docker-compose.yml
-func PullAllImages(opts BaseOptions) error {
-	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "pull"}
-	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
-}
-
-// RunImages runs the given docker images
-func RunImages(opts ImagesOptions) error {
-	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "up"}
-	if opts.AbortOnExit {
-		cmd = append(cmd, "--abort-on-container-exit")
-	}
 	cmd = append(cmd, opts.ImageNames...)
 	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
 }
 
-// RunImage runs a given image
-func RunImage(opts ImagesOptions, imageName string) error {
+// KillContainers kills images in opts.ImageNames, or kills all images if opts.ImageNames is empty
+func KillContainers(opts CommandOptions) error {
+	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "down"}
+	cmd = append(cmd, opts.ImageNames...)
+	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
+}
+
+// PullImages pulls images in opts.ImageNames, or pulls all images if opts.ImageNames is empty
+func PullImages(opts CommandOptions) error {
+	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "pull"}
+	cmd = append(cmd, opts.ImageNames...)
+	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
+}
+
+// RunImages runs the given docker images, or runs all images if opts.ImageNames is empty
+func RunImages(opts CommandOptions) error {
 	cmd := []string{"docker-compose", "--file", opts.DockerComposeFileName, "up"}
 	if opts.AbortOnExit {
 		cmd = append(cmd, "--abort-on-container-exit")
 	}
-	cmd = append(cmd, imageName)
+	if opts.Build {
+		cmd = append(cmd, "--build")
+	}
+	cmd = append(cmd, opts.ImageNames...)
 	return util.RunAndPipe(opts.DockerComposeDir, opts.Env, opts.Writer, cmd...)
 }
