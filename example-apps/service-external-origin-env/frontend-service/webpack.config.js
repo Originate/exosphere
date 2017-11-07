155ce1c91cea6ae279b5bd8b1d5f9784f81fbadf
Charles Rudolph <charles.rudolph@originate.com>
2017-11-07 11:35:55 -0800

    update

---
+++ example-apps/service-external-origin-env/frontend-service/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      backendUrl: process.env.SERVICE_BACKEND_EXTERNAL_ORIGIN,
+      template: 'src/index.pug',
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      loader: 'pug-loader',
+    }],
+  },
+}
