42d00c4e9a814dd2a9a6f157dd39d02fd7b7c706
Charles Rudolph <charles.rudolph@originate.com>
2017-11-09 10:09:07 -0800

    update

--- src/types/development_dependency_config_options.go
+++ src/types/development_dependency_config_options.go
@@ -7,11 +7,3 @@ type DevelopmentDependencyConfigOptions struct {
 	DependencyEnvironment map[string]string `yaml:"dependency-environment,omitempty"`
 	ServiceEnvironment    map[string]string `yaml:"service-environment,omitempty"`
 }
-
-// IsEmpty returns true if the given dependencyConfig object is empty
-func (d *DevelopmentDependencyConfigOptions) IsEmpty() bool {
-	return len(d.Ports) == 0 &&
-		len(d.Volumes) == 0 &&
-		len(d.DependencyEnvironment) == 0 &&
-		len(d.ServiceEnvironment) == 0
-}
