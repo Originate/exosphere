8d56babf1cd3cb5bbdc857f0eb96db5fd79eb343
Hugo Ho <hugobho@users.noreply.github.com>
2017-11-27 16:09:22 -0800

    address comment

--- src/terraform/commands.go
+++ src/terraform/commands.go
@@ -14,13 +14,13 @@ func RunInit(deployConfig types.DeployConfig) error {
 }
 
 // RunApply runs the 'terraform apply' command and passes variables in as command flags
-func RunApply(deployConfig types.DeployConfig, secrets types.Secrets, imagesMap map[string]string, autoapprove bool) error {
+func RunApply(deployConfig types.DeployConfig, secrets types.Secrets, imagesMap map[string]string, autoApprove bool) error {
 	vars, err := CompileVarFlags(deployConfig, secrets, imagesMap)
 	if err != nil {
 		return err
 	}
 	command := append([]string{"terraform", "apply"}, vars...)
-	if autoapprove {
+	if autoApprove {
 		command = append(command, "-auto-approve")
 	}
 	return util.RunAndPipe(deployConfig.TerraformDir, []string{}, deployConfig.Writer, command...)
