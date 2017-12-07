14a3286dedb1dbf19c760a4778ea96c9956bf29f
Hugo <hugo.ho@originate.com>
2017-12-07 09:40:20 -0800

    address comment

--- src/types/service_development_config.go
+++ src/types/service_development_config.go
@@ -2,7 +2,6 @@ package types
 
 // ServiceDevelopmentConfig represents development specific configuration for a service
 type ServiceDevelopmentConfig struct {
-	HealthCheck string            `yaml:"health-check,omitempty"`
-	Port        string            `yaml:",omitempty"`
-	Scripts     map[string]string `yaml:",omitempty"`
+	Port    string            `yaml:",omitempty"`
+	Scripts map[string]string `yaml:",omitempty"`
 }
