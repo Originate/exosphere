2e6cc954c40983a1acaefd678cad2645949308e6
Hugo Ho <hugobho@users.noreply.github.com>
2017-12-06 16:57:46 -0800

    remove environments block

--- src/types/env_vars.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// EnvVars is the environment key listed in a ServiceConfig
-type EnvVars struct {
-	Default map[string]string `yaml:",omitempty"`
-	Local   map[string]string `yaml:",omitempty"`
-	Remote  map[string]string `yaml:",omitempty"`
-	Secrets []string          `yaml:",omitempty"`
-}
--- src/types/local_config.go
+++ src/types/local_config.go
@@ -3,4 +3,6 @@ package types
 // LocalConfig represents development specific configuration for an application
 type LocalConfig struct {
 	Dependencies []LocalDependency
+	Environment  map[string]string `yaml:",omitempty"`
+	Secrets      []string          `yaml:",omitempty"`
 }
