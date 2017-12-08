e772697b8fa37b0b33c409e4669ffe883ee50e1c
Hugo Ho <hugobho@users.noreply.github.com>
2017-12-08 11:47:03 -0800

    update

--- src/types/remote_dependency.go
+++ src/types/remote_dependency.go
@@ -14,13 +14,13 @@ type RemoteDependency struct {
 
 // ValidateFields validates that a remote config contains all required fields
 func (p *RemoteDependency) ValidateFields() error {
-	if p.Type == "rds" {
+	switch p.Type {
+	case "rds":
 		err := p.Config.Rds.ValidateFields()
 		if err != nil {
 			return errors.Wrap(err, fmt.Sprintf("remote dependency %s has issues", p.Type))
 		}
-	}
-	if p.Type == "exocom" {
+	case "exocom":
 		if p.Config.Version == "" {
 			return errors.New("exocom dependency missing 'version' field")
 		}
