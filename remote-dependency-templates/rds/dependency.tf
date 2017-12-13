e01f9cab2dab91ee128d476f02b53db9a6034c57
Hugo Ho <hugobho@users.noreply.github.com>
2017-12-12 16:34:52 -0800

    update terraform template

--- remote-dependency-templates/rds/dependency.tf
+++ remote-dependency-templates/rds/dependency.tf
@@ -1,20 +1,20 @@
-variable "{{passwordSecretName}}" {}
+variable "{{password-secret-name}}" {}
 
-module "{{name}}_rds_instance" {
+module "{{db-name}}_rds_instance" {
   source = "github.com/Originate/exosphere.git//terraform//aws//dependencies//rds?ref={{terraformCommitHash}}"
 
-  allocated_storage       = "{{allocatedStorage}}"
+  allocated_storage       = "{{allocated-storage}}"
   ecs_security_group      = "${module.aws.ecs_cluster_security_group}"
   bastion_security_group  = "${module.aws.bastion_security_group}"
   engine                  = "{{engine}}"
-  engine_version          = "{{engineVersion}}"
+  engine_version          = "{{engine-version}}"
   env                     = "production"
-  instance_class          = "{{instanceClass}}"
+  instance_class          = "{{instance-class}}"
   internal_hosted_zone_id = "${module.aws.internal_zone_id}"
-  name                    = "{{name}}"
+  name                    = "{{db-name}}"
   username                = "{{username}}"
-  password                = "${var.{{passwordSecretName}}}"
-  storage_type            = "{{storageType}}"
+  password                = "${var.{{password-secret-name}}}"
+  storage_type            = "{{storage-type}}"
   subnet_ids              = ["${module.aws.private_subnet_ids}"]
   vpc_id                  = "${module.aws.vpc_id}"
 }
