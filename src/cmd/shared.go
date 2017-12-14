0f747485f7be577dd16cfb70c4498ce755e73904
Charlie Rudolph <charles.w.rudolph@gmail.com>
2017-12-14 12:35:10 -0700

    deploy config: clean up terraform dir (#878)

--- src/cmd/shared.go
+++ src/cmd/shared.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"path/filepath"
 
 	"github.com/Originate/exosphere/src/aws"
 	"github.com/Originate/exosphere/src/docker/composebuilder"
@@ -48,12 +47,9 @@ func getSecrets(awsConfig types.AwsConfig) types.Secrets {
 
 func getBaseDeployConfig(appContext *context.AppContext) deploy.Config {
 	awsConfig := getAwsConfig(appContext.Config, deployProfileFlag)
-	terraformDir := filepath.Join(appContext.Location, "terraform")
 	return deploy.Config{
 		AppContext:               appContext,
 		DockerComposeProjectName: composebuilder.GetDockerComposeProjectName(appContext.Config.Name),
-		TerraformDir:             terraformDir,
-		SecretsPath:              filepath.Join(terraformDir, "secrets.tfvars"),
 		AwsConfig:                awsConfig,
 		BuildMode: types.BuildMode{
 			Type:        types.BuildModeTypeDeploy,
--- src/types/deploy/deploy_config.go
+++ src/types/deploy/deploy_config.go
@@ -2,6 +2,7 @@ package deploy
 
 import (
 	"io"
+	"path"
 
 	"github.com/Originate/exosphere/src/types"
 	"github.com/Originate/exosphere/src/types/context"
@@ -13,8 +14,16 @@ type Config struct {
 	BuildMode                types.BuildMode
 	Writer                   io.Writer
 	DockerComposeProjectName string
-	TerraformDir             string
-	SecretsPath              string
 	AwsConfig                types.AwsConfig
 	AutoApprove              bool
+}
+
+// GetTerraformDir returns the path of the terraform directory
+func (c Config) GetTerraformDir() string {
+	return path.Join(c.AppContext.Location, c.GetRelativeTerraformDir())
+}
+
+// GetRelativeTerraformDir returns the relative path of the terraform directory
+func (c Config) GetRelativeTerraformDir() string {
+	return "terraform"
 }
