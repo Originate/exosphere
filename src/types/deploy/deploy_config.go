b2c8c5609db89b768e106c014a06ce90035bbece
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-12-13 16:02:06 -0800

    wip

--- src/types/deploy/deploy_config.go
+++ src/types/deploy/deploy_config.go
@@ -2,21 +2,45 @@ package deploy
 
 import (
 	"io"
+	"path/filepath"
 
+	"github.com/Originate/exosphere/src/docker/composebuilder"
 	"github.com/Originate/exosphere/src/types"
 	"github.com/Originate/exosphere/src/types/context"
 )
 
 // Config contains information needed for deployment
 type Config struct {
-	AppContext               *context.AppContext
-	BuildMode                types.BuildMode
-	Writer                   io.Writer
-	DockerComposeProjectName string
-	DockerComposeDir         string
-	TerraformDir             string
-	SecretsPath              string
-	AwsConfig                types.AwsConfig
-	AutoApprove              bool
-	TerraformModulesRef      string
+	AppContext   *context.AppContext
+	BuildMode    types.BuildMode
+	Writer       io.Writer
+	TerraformDir string
+	AwsConfig    types.AwsConfig
+	AutoApprove  bool
 }
+
+func (c Config) GetDockerComposeProjectName() string {
+	return composebuilder.GetDockerComposeProjectName(c.AppContext.Config.Name)
+}
+
+func (c Config) GetBuildMode() types.BuildMode {
+	return types.BuildMode{
+		Type:        types.BuildModeTypeDeploy,
+		Environment: types.BuildModeEnvironmentProduction,
+	}
+}
+
+func (c Config) GetSecretsPath() string {
+	return filepath.Join(c.TerraformDir, "secrets.tfvars")
+}
+
+// AppContext:               appContext,
+// DockerComposeProjectName: composebuilder.GetDockerComposeProjectName(appContext.Config.Name),
+// DockerComposeDir:         path.Join(appContext.Location, "docker-compose"),
+// TerraformDir:             terraformDir,
+// SecretsPath:              filepath.Join(terraformDir, "secrets.tfvars"),
+// AwsConfig:                awsConfig,
+// BuildMode: types.BuildMode{
+// 	Type:        types.BuildModeTypeDeploy,
+// 	Environment: types.BuildModeEnvironmentProduction,
+// },
