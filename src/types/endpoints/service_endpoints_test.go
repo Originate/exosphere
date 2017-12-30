d7a9bd920fe75e019689a304be3b46c537754e3c
Hugo Ho <hugobho@users.noreply.github.com>
2017-12-29 17:17:20 -0800

    address comments

--- src/types/endpoints/service_endpoints_test.go
+++ src/types/endpoints/service_endpoints_test.go
@@ -27,15 +27,15 @@ var _ = Describe("ServiceEndpoints", func() {
 		}
 		serviceEndpoints := endpoints.NewServiceEndpoints(appContext, buildMode, "")
 		envVars := serviceEndpoints.GetServiceEndpointEnvVars("users")
-		Expect(envVars["WEB_EXTERNAL_ORIGIN"]).To(Equal("http://localhost:3010"))
+		Expect(envVars["WEB_EXTERNAL_ORIGIN"]).To(Equal("http://localhost:3000"))
 		Expect(envVars["WEB_INTERNAL_ORIGIN"]).To(Equal("http://web:4000"))
 		mapping := serviceEndpoints.GetServicePortMappings("web")
-		Expect(mapping[0]).To(Equal("3010:4000"))
+		Expect(mapping[0]).To(Equal("3000:4000"))
 
 		envVars = serviceEndpoints.GetServiceEndpointEnvVars("web")
 		Expect(envVars["USERS_HOST"]).To(Equal("http://users:2121"))
 		mapping = serviceEndpoints.GetServicePortMappings("users")
-		Expect(mapping[0]).To(Equal("3000:2121"))
+		Expect(len(mapping)).To(Equal(0))
 	})
 
 	It("compiles the proper local production endpoints", func() {
@@ -46,14 +46,14 @@ var _ = Describe("ServiceEndpoints", func() {
 		}
 		serviceEndpoints := endpoints.NewServiceEndpoints(appContext, buildMode, "qa")
 		envVars := serviceEndpoints.GetServiceEndpointEnvVars("users")
-		Expect(envVars["WEB_EXTERNAL_ORIGIN"]).To(Equal("http://localhost:3010"))
+		Expect(envVars["WEB_EXTERNAL_ORIGIN"]).To(Equal("http://localhost:3000"))
 		Expect(envVars["WEB_INTERNAL_ORIGIN"]).To(Equal("http://web:80"))
 		mapping := serviceEndpoints.GetServicePortMappings("web")
-		Expect(mapping[0]).To(Equal("3010:80"))
+		Expect(mapping[0]).To(Equal("3000:80"))
 
 		envVars = serviceEndpoints.GetServiceEndpointEnvVars("web")
 		Expect(envVars["USERS_HOST"]).To(Equal("http://users:21"))
 		mapping = serviceEndpoints.GetServicePortMappings("users")
-		Expect(mapping[0]).To(Equal("3000:21"))
+		Expect(len(mapping)).To(Equal(0))
 	})
 })
