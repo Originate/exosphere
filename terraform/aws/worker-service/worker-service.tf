3686140a6340474d57bdbeb1b30774fd93e19996
Hugo Ho <hugobho@users.noreply.github.com>
2018-01-04 10:52:35 -0800

    update

--- terraform/aws/worker-service/worker-service.tf
+++ terraform/aws/worker-service/worker-service.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_service" "service" {
 }
 
 resource "aws_iam_role" "allow_dns_changes" {
-  name = "ecs-allow-dns-changes"
+  name = "${var.name}-ecs-allow-dns-changes"
 
   assume_role_policy = <<EOF
 {
@@ -40,7 +40,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "allow_dns_changes" {
-  name = "ecs-allow-dns-changes-role-policy"
+  name = "${var.name}-ecs-allow-dns-changes-role-policy"
   role = "${aws_iam_role.allow_dns_changes.id}"
 
   policy = <<EOF
