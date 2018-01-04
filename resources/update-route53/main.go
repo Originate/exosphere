983de50be8afa34a69ff410d70c506cf8007aee2
Hugo Ho <hugobho@users.noreply.github.com>
2018-01-04 13:59:20 -0800

    update

--- resources/update-route53/main.go
+++ resources/update-route53/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,15 +12,14 @@ import (
 )
 
 func main() {
-	// config := &aws.Config{
-	// 	Region: aws.String("us-west-2"),
-	// 	Credentials: credentials.NewCredentials(&credentials.ChainProvider{
-	// 		Providers: []credentials.Provider{
-	// 			&credentials.EnvProvider{},
-	// 			&credentials.SharedCredentialsProvider{Profile: "space-tweet"},
-	// 		},
-	// 	}),
-	// }
+	if len(os.Arg) < 3 {
+		panic(errors.New("Not enough arguments. Arguments must be non-empty strings passed into 'route53-updater <service-role> <internal-hosted-zone-name>'"))
+	}
+	serviceRole := os.Args[1]
+	internalHostedZoneName := os.Args[2]
+	if serviceRole == "" || internalHostedZoneName == "" {
+		panic(errors.New("Service role or internal hosted zone name missing. Both arguments must be non-empty strings passed into 'route53-updater <service-role> <internal-hosted-zone-name>'"))
+	}
 	internalIP, err := exec.Command("curl", "-fsSL", "http://169.254.169.254/latest/meta-data/local-ipv4").Output()
 	if err != nil {
 		panic(err)
@@ -30,11 +31,17 @@ func main() {
 		panic(err)
 	}
 	var hostedZoneId *string
+	var hostedZoneFound bool
 	for _, hostedZone := range hostedZones.HostedZones {
-		if *hostedZone.Name == "space-tweet.local." {
+		if *hostedZone.Name == fmt.Sprintf("%s.", internalHostedZoneName) {
 			hostedZoneId = hostedZone.Id
+			hostedZoneFound = true
 		}
 	}
+	if !hostedZoneFound {
+		panic(fmt.Errorf("Hosted zone name '%s' not found.", internalHostedZoneName))
+	}
+
 	changeRecordResult, err := awsClient.ChangeResourceRecordSets(&route53.ChangeResourceRecordSetsInput{
 		HostedZoneId: hostedZoneId,
 		ChangeBatch: &route53.ChangeBatch{
@@ -43,11 +50,11 @@ func main() {
 				{
 					Action: aws.String("UPSERT"),
 					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String("test.space-tweet.local"),
+						Name: aws.String(fmt.Sprintf("%s.%s", serviceRole, internalHostedZoneName)),
 						Type: aws.String("A"),
 						TTL:  aws.Int64(int64(300)),
 						ResourceRecords: []*route53.ResourceRecord{
-							{Value: aws.String(internalIP)},
+							{Value: aws.String(string(internalIP))},
 						},
 					},
 				},
@@ -57,5 +64,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(changeRecordResult)
 }
